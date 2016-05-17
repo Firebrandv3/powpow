f33f854584d0b6165fc0960c7983479c9a9748c1
Erfan Ebrahimnia <eebrahimnia.mmt-b2015@fh-salzburg.ac.at>
2016-05-18 00:52:18 +0200

    update for heroku

--- src/client/js/states/preload.js
+++ src/client/js/states/preload.js
@@ -15,16 +15,16 @@ Preload.prototype.preload = function() {
 
 Preload.prototype.loadAssets = function() {
     // images
-    this.load.image('grid', 'game-assets/grid.jpg');
-    this.load.image('weapon', 'game-assets/sprites/weapon.png', 64, 56);
-    this.load.image('healthPickup', 'game-assets/sprites/health.png', 24, 20);
-    this.load.image('ammoPickup', 'game-assets/sprites/ammo.png', 13, 24);
-    this.load.image('death', 'game-assets/sprites/death.png', 100, 100);
-    this.load.image('dickbutt', 'game-assets/sprites/dickbutt.png', 422, 424);
-    this.load.image('sun', 'game-assets/sprites/sun.png', 324, 324);
-    this.load.spritesheet('character', 'game-assets/sprites/character.png', 100, 140, 6);
-    this.load.spritesheet('projectile', 'game-assets/sprites/projectile.png', 43, 64, 6);
-    this.load.spritesheet('jetpack', 'game-assets/sprites/jetpack_spritesheet.png', 40, 120, 3);
+    this.load.image('grid', '/game-assets/grid.jpg');
+    this.load.image('weapon', '/game-assets/sprites/weapon.png', 64, 56);
+    this.load.image('healthPickup', '/game-assets/sprites/health.png', 24, 20);
+    this.load.image('ammoPickup', '/game-assets/sprites/ammo.png', 13, 24);
+    this.load.image('death', '/game-assets/sprites/death.png', 100, 100);
+    this.load.image('dickbutt', '/game-assets/sprites/dickbutt.png', 422, 424);
+    this.load.image('sun', '/game-assets/sprites/sun.png', 324, 324);
+    this.load.spritesheet('character', '/game-assets/sprites/character.png', 100, 140, 6);
+    this.load.spritesheet('projectile', '/game-assets/sprites/projectile.png', 43, 64, 6);
+    this.load.spritesheet('jetpack', '/game-assets/sprites/jetpack_spritesheet.png', 40, 120, 3);
 };
 
 Preload.prototype.onLoadCompleteHandler = function() {
